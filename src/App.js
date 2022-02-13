46cac4008f91b432f664f5554626b0bde9c61ceb
emilio-v <emilio_785@hotmail.com>
2022-02-13 12:02:03 -0600

    create routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <h1>MERNTasks</h1>;
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./components/auth/Login";
+import NewAccount from "./components/auth/NewAccount";
+import Proyects from "./components/proyects/Proyects";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/new-account" element={<NewAccount />} />
+        <Route exact path="/proyects" element={<Proyects />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
