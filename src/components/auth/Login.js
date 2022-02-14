aec29b1dcb995152f9e5389188fd8cbfaddaadbd
emilio-v <emilio_785@hotmail.com>
2022-02-13 23:06:15 -0600

    created login form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = user;
+
+  const onChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    // validate empty fields
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1 className="">Log In</h1>
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              onChange={onChange}
+              value={email}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              onChange={onChange}
+              value={password}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Log In!"
+            />
+          </div>
+        </form>
+        <Link to="/new-account" className="enlace-cuenta text-center">
+          Sign up
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
