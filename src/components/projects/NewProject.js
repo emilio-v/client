c0deff94be2a6e9c9f86742c24dd8de45dce10c0
emilio-v <emilio_785@hotmail.com>
2022-02-14 01:11:44 -0600

    added onChange and onSubmit to form

--- src/components/projects/NewProject.js
+++ src/components/projects/NewProject.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewProject = () => {
+  const [project, setProject] = useState({
+    name: "",
+  });
+
+  const { name } = project;
+
+  const onChangeProject = (e) => {
+    setProject({ ...project, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <button type="button" className="btn btn-block btn-primario">
         New Project
       </button>
-      <form className="formulario-nuevo-proyecto">
+      <form onSubmit={onSubmit} className="formulario-nuevo-proyecto">
         <input
           type="text"
           className="input-text"
           placeholder="Project Name"
           name="name"
+          onChange={onChangeProject}
+          value={name}
         />
         <input
           type="submit"
